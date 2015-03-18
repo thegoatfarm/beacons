75b17e4cc0151005ddb9a0a77d0e36289d68c67a
ericrav <ericrabinowitz@me.com>
2015-03-17 20:16:42 -0400

    particles on load

--- js/main.js
+++ js/main.js
@@ -24,6 +24,7 @@ function showPage() {
     $(".main").fadeIn();
     $(".logo").fadeIn();
     $("#name").css("display", "inline-block");
+    particles();
     $(".computer .text").typed({
             strings: ["Hello.", "To get started, may I have your name?"],
             typeSpeed: 10,
@@ -97,64 +98,66 @@ function takeBreak() {
 
 
 
-particlesJS('particles-js', {
-  particles: {
-    color: '#bc0c11',
-    color_random: false,
-    shape: 'circle', // "circle", "edge" or "triangle"
-    opacity: {
-      opacity: 1,
-      anim: {
-        enable: false,
-        speed: 1.5,
-        opacity_min: 0,
-        sync: false
-      }
-    },
-    size: 2.5,
-    size_random: false,
-    nb: 30,
-    line_linked: {
-      enable_auto: true,
-      distance: 140,
-      color: '#bc0c11',
-      opacity: 1,
-      width: 1,
-      condensed_mode: {
-        enable: false,
-        rotateX: 600,
-        rotateY: 600
-      }
-    },
-    anim: {
-      enable: true,
-      speed: 1
-    }
-  },
-  interactivity: {
-    enable: true,
-    mouse: {
-      distance: 250
-    },
-    detect_on: 'window', // "canvas" or "window"
-    mode: 'grab', // "grab" of false
-    line_linked: {
-      opacity: .5
-    },
-    events: {
-      onclick: {
-        enable: false,
-        mode: 'push', // "push" or "remove"
-        nb: 4
+function particles() {
+    particlesJS('particles-js', {
+      particles: {
+        color: '#bc0c11',
+        color_random: false,
+        shape: 'circle', // "circle", "edge" or "triangle"
+        opacity: {
+          opacity: 1,
+          anim: {
+            enable: false,
+            speed: 1.5,
+            opacity_min: 0,
+            sync: false
+          }
+        },
+        size: 2.5,
+        size_random: false,
+        nb: 30,
+        line_linked: {
+          enable_auto: true,
+          distance: 140,
+          color: '#bc0c11',
+          opacity: 1,
+          width: 1,
+          condensed_mode: {
+            enable: false,
+            rotateX: 600,
+            rotateY: 600
+          }
+        },
+        anim: {
+          enable: true,
+          speed: 1
+        }
       },
-      onresize: {
+      interactivity: {
         enable: true,
-        mode: 'out', // "out" or "bounce"
-        density_auto: false,
-        density_area: 800 // nb_particles = particles.nb * (canvas width *  canvas height / 1000) / density_area
-      }
-    }
-  },
-  /* Retina Display Support */
-  retina_detect: true
-});
+        mouse: {
+          distance: 250
+        },
+        detect_on: 'window', // "canvas" or "window"
+        mode: 'grab', // "grab" of false
+        line_linked: {
+          opacity: .5
+        },
+        events: {
+          onclick: {
+            enable: false,
+            mode: 'push', // "push" or "remove"
+            nb: 4
+          },
+          onresize: {
+            enable: true,
+            mode: 'out', // "out" or "bounce"
+            density_auto: false,
+            density_area: 800 // nb_particles = particles.nb * (canvas width *  canvas height / 1000) / density_area
+          }
+        }
+      },
+      /* Retina Display Support */
+      retina_detect: true
+    });
+}
